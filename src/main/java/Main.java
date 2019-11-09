3a0d1a93aede8369a55271aa852b86060d867615
Ivan <IvanD18@mail.ru>
2019-11-09 16:33:12 +0300

    First HW with one class (Main)

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int fuelAmount = 30;
+        int fuelRate = 7;
+        int distanceUnit = 100;
+        System.out.println(fuelAmount * distanceUnit / fuelRate);
+    }
+}
